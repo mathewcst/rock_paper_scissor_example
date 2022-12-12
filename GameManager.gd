1ac8292148ba5276249eaf9e3c0c2da9d7ffbcda
Matheus Costa <mathfernandesz@gmail.com>
2022-12-12 03:57:30 -0300

    simple button choices

---
+++ GameManager.gd
@@ -0,0 +1,4 @@
+extends Node
+
+var player_one_choice: String = ""
+var player_two_choice: String = ""
---
+++ PickCard.gd
@@ -0,0 +1,14 @@
+extends CanvasLayer
+
+
+
+func _on_Rock_button_down() -> void:
+	print('You chose Rock')
+
+
+func _on_Paper_button_down() -> void:
+	print('You chose Paper')
+
+
+func _on_Scissor_button_down() -> void:
+	print('You chose Scissor')
